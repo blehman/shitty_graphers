139ae6be487627be844e40a6996e4e6253190138
blehman <the.lehman.report@gmail.com>
2015-12-02 13:26:28 -0700

    Adding re-useable chart example

--- js/heatmap.js
+++ js/heatmap.js
@@ -51,13 +51,14 @@
 // add elements in which to place the chart
 function heatmap(){
   var margin = { top: 50, right: 0, bottom: 100, left: 130 }
-    , width = 1500 - margin.left - margin.right
-    , height = 2000 - margin.top - margin.bottom
+    , width =1500 - margin.left - margin.right
+    , height = 1500 - margin.top - margin.bottom
     , gridSize = Math.floor(width / 24)
     , legendElementWidth = gridSize*2
     , buckets = 9
-    , colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"] // alternatively colorbrewer.YlGnBu[9]
-    , colorScale = d3.scale.linear().domain([-2,2]).range(['#990000','#ff9900']);
+    , colors = ['#990000','#ff9900']
+    , colorDomain = [-2,2]
+    , colorScale = d3.scale.linear().domain(colorDomain).range(colors);
 
   function chart(selection){
     selection.each(function(data){
@@ -99,14 +100,63 @@ function heatmap(){
             .attr("transform", "translate(" + gridSize / 2 + ", -6)");
             //.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis"); });
 
-/*       var colorScale = d3.scale.quantile()
-           .domain([0, columns - 1, d3.max(data, function (d) { return d.value; })])
-           .range(colors);
-*/
         var rowElement = svg.append('g')
             .classed('row',true);
 
-         data.forEach(function(d,i){
+        var myRows = rowElement.selectAll('g')
+            .data(data).enter()
+            .append('g')
+            .classed('test_rows',true);
+
+
+        // add rectangles WITH a data binding
+
+        var gTest = svg.append('g')
+            .classed('gTest',true);
+
+        // notice that we bind the data here and use a transform to adjust the y-value for each g. 
+            // note: each g represents one row.
+        var testGs = gTest.selectAll('g')
+            .data(data)
+            .enter()
+            .append('g')
+            .attr("transform", function(d,i) { return "translate(0," + (i * gridSize) + ")";});
+
+        // create a selection that is ready to bind with data
+            // note: d3.entries() breaks the object into an array of row elements each representing an item in that column.
+        var dataBoundElements = testGs.selectAll("rect")
+            .data(function(d,i) { 
+              return d3.entries(d).filter(function(e) { return e.key != "row_name";});
+            });
+
+        // create the rectangles.
+        var rectElements = dataBoundElements
+            .enter().append("rect")
+            .attr({
+              x: function(d,i) { return i * gridSize;}
+            , y: 0
+            , width: gridSize
+            , height: gridSize
+            , class:"cell"
+            })
+            .style('fill',colors[0])
+            .transition().duration(3000)
+            .style('fill',function(d) {
+              return colorScale( +d.value )} );
+
+        // create text in the elements
+        var textElements = dataBoundElements
+            .enter().append('text')
+            .attr({
+              x: function(d,i) { return i * gridSize + 10;}
+            , y: 0.5 * gridSize
+            , class:"cellText"
+            })
+            .text(function(d){return Math.round10(+d.value,-1)});
+
+        /*
+        // add rectangles WITHOUT a data binding 
+        data.forEach(function(d,i){
             for (col in d3.range(columns)){
                 rowElement.append('rect')
                     .attr("x", function(d) { return (+col) * gridSize; })
@@ -131,34 +181,45 @@ function heatmap(){
 
             }
          })
-/*
-         d3.selectAll('.transition().duration(1000)
-            .style("fill", function(d) { return colorScale(d.value); });
 
-         cards.select("title").text(function(d) { return d.value; });
+        */ 
 
-         cards.exit().remove();
-*/
+         // create legend
+         var legend = svg.append('g')
+            .classed('legend',true)
+            .attr("transform", function(d,i) { return "translate(0," + (height + 20 ) + ")";});
+
+         console.log((colorDomain[1]-colorDomain[0]) / columns)
+         legend.selectAll('.legend_rect')
+            .data(d3.range(colorDomain[0],colorDomain[1], (colorDomain[1]-colorDomain[0]) / columns ))
+            .enter().append('rect')
+            .attr({
+              x: function(d,i){ return gridSize * i }
+            , y: 0
+            , height: gridSize
+            , width: gridSize
+            })
+            .classed('cell',true)
+            .style('fill', function(d){
+              return colorScale( +d )} );
+
+
 /*
          var legend = svg.selectAll(".legend")
             .data([0].concat(colorScale.quantiles()), function(d) { return d; });
-
          legend.enter().append("g")
             .attr("class", "legend");
-
          legend.append("rect")
              .attr("x", function(d, i) { return legendElementWidth * i; })
              .attr("y", height)
              .attr("width", legendElementWidth)
              .attr("height", gridSize / 2)
              .style("fill", function(d, i) { return colors[i]; });
-
          legend.append("text")
              .attr("class", "mono")
              .text(function(d) { return "≥ " + Math.round(d); })
              .attr("x", function(d, i) { return legendElementWidth * i; })
              .attr("y", height + gridSize);
-
          legend.exit().remove();
 */
     })
@@ -206,7 +267,12 @@ function heatmap(){
     return chart;
   };
 
+  chart.colorDomain = function(d) {
+    if (!arguments.length) { return colorDomain; }
+    colorDomain = d;
+    return chart;
+  };
+
   return chart
 }
-
 
